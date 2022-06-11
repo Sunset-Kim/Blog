fc72389fd980478afc9e6284e629372f35b4f80d
Sunset-kim <wood2002@naver.com>
2022-06-11 18:44:31 +0900

    튜토리얼3 : gatsby plugin 사용법, image 이용하기

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,14 +1,12 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
+    title: "민우의개발블로그",
     siteUrl: `https://www.yourdomain.tld`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
 
-export default config
+export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 import Layout from "../components/layouts/Layout";
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle="Home Page">
+        <StaticImage
+          src="https://static.wikia.nocookie.net/pokemon/images/5/52/%ED%94%BC%EC%B9%B4%EC%B8%84_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest/scale-to-width-down/200?cb=20170405000019&path-prefix=ko"
+          alt="카피츄"
+        />
+        <StaticImage src="../assets/picka.png" alt="피카츄" />
         <p>I'm making this by following the Gatsby Tutorial.</p>
       </Layout>
     </main>

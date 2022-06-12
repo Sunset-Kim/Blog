73c95c002ea7939ac541b5469b8520236500e475
Sunset-kim <wood2002@naver.com>
2022-06-12 17:31:11 +0900

    3. templelate를 이용해 블로그 포스트를 렌더링해보기

---
+++ src/templates/blog-post.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layouts/Layout";
+
+interface BlogPostProps {
+  data: {
+    markdownRemark: {
+      frontmatter: {
+        title: string;
+        datePublished: string;
+        author: string;
+      };
+      html: string;
+    };
+  };
+}
+
+export default function BlogPost({ data }: BlogPostProps) {
+  const post = data.markdownRemark;
+  return (
+    <Layout pageTitle={post.frontmatter.title}>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  );
+}
+export const query = graphql`
+  query ($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        datePublished(formatString: "MM DD dddd,YYYY", locale: "ko")
+        author
+      }
+    }
+  }
+`;

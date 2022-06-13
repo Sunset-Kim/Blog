8578aab2fcd3f308c070986a9ca12ea22ed21eb1
Sunset-kim <wood2002@naver.com>
2022-06-13 21:48:25 +0900

    add : 외부 경로에 이미지를 삽입하기

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
 import Layout from "@components/layouts/Layout";
+import { GatsbyImage, getImage, IGatsbyImageData } from "gatsby-plugin-image";
+import "@styles/code-copy-button.css";
 
 interface BlogPostProps {
   data: {
@@ -9,6 +11,10 @@ interface BlogPostProps {
         title: string;
         datePublished: string;
         author: string;
+        image: IGatsbyImageData;
+        image_alt: string;
+        image_credit_link: string;
+        image_credit_text: string;
       };
       html: string;
     };
@@ -17,8 +23,24 @@ interface BlogPostProps {
 
 export default function BlogPost({ data }: BlogPostProps) {
   const post = data.markdownRemark;
+  const image = getImage(post.frontmatter.image);
+  const isImgExist =
+    post.frontmatter.image &&
+    post.frontmatter.image_alt &&
+    post.frontmatter.image_credit_link &&
+    post.frontmatter.image_credit_text;
+
   return (
     <Layout pageTitle={post.frontmatter.title}>
+      {isImgExist && (
+        <>
+          <GatsbyImage image={image} alt={post.frontmatter.image_alt} />
+          <p>
+            Photo Credit: <a href={post.frontmatter.image_credit_link}>{post.frontmatter.image_credit_text}</a>
+          </p>
+        </>
+      )}
+
       <div>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
@@ -33,6 +55,14 @@ export const query = graphql`
         title
         datePublished(formatString: "MM DD dddd,YYYY", locale: "ko")
         author
+        image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        image_alt
+        image_credit_link
+        image_credit_text
       }
     }
   }

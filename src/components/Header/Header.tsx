3d1748488a006ee6abd53df356d21909f3cd34e0
Sunset-kim <wood2002@naver.com>
2022-06-22 23:06:25 +0900

    style : 스타일 컴포넌트 및 폰트 적용

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,12 @@
 import React, { PropsWithChildren } from "react";
+import styled from "@emotion/styled";
 
 const Header: React.FC<PropsWithChildren> = ({ children }) => {
-  return <header>{children}</header>;
+  return <CONTAINER>{children}</CONTAINER>;
 };
+
+const CONTAINER = styled.header`
+  padding: 16px 0;
+`;
 
 export default Header;
--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@components/Header/Header";
+import styled from "@emotion/styled";
 import { Link, useStaticQuery, graphql } from "gatsby";
 import React from "react";
 
@@ -19,30 +20,34 @@ const Layout: React.FC<LayoutProps & React.PropsWithChildren> = ({ pageTitle, ch
   `);
 
   return (
-    <div>
+    <WRAPPER>
       <title>
         {pageTitle} | {site.siteMetadata.title}
       </title>
-      <Header>{site.siteMetadata.title}</Header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-        </ul>
-      </nav>
+      <Header>
+        {site.siteMetadata.title}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/blog">블로그 톺아보기</Link>
+            </li>
+          </ul>
+        </nav>
+      </Header>
+
       <main>
         <h1>{pageTitle}</h1>
         {children}
       </main>
-    </div>
+    </WRAPPER>
   );
 };
+
+const WRAPPER = styled.div`
+  margin: 0 auto;
+  padding: 0 48px;
+  min-width: 1024px;
+  max-width: 1200px;
+`;
 
 export default Layout;
